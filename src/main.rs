0e01d6dfa56b7aff783eac2bd41d1a0d446b932e
Gabriel Franke <74662066+zenyaboi@users.noreply.github.com>
2023-12-19 13:23:34 -0300

    added new libraries

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 // allow unused variables (using this removes warnings of unused variables)
 #![allow(unused)]
 
+// adding libraries
+use std::io;
+use rand::Rng;
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
 fn main() {
     println!("Hello, world!");
 }
